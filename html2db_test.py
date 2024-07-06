5f00790b0587c8219cac19eb847c8e64fd42e534
Philip Lessner <philiplessner@gmail.com>
2024-07-05 21:06:20 -0400

    Made a list of more K EMET technical blogs. html2db now accepts file path as argument

--- html2db_test.py
+++ html2db_test.py
@@ -1,10 +1,12 @@
+import sys
 from html_ingest import HTMLIngest
 
-with open("./data/KEMET_FAQs.txt", "r") as f:
+filename = sys.argv[1]
+with open(filename, "r") as f:
     files = [line.strip('\n') for line in f]
 
 for file in files:
-    test = HTMLIngest(file, 'div', 'datasheet-content')
+    test = HTMLIngest(file, 'div', 'cmp-container')
     test.html2elements()
     test.elements2chunks()
     test.chunks2docs()
