a5b76bbee5b262bcdb4706a9b41c9070c2271461
Philip Lessner <philiplessner@gmail.com>
2024-07-06 20:59:34 -0400

    Cleaned up web questions. Added some Pulse pdfs to database

--- pdf2db_test.py
+++ pdf2db_test.py
@@ -1,15 +1,15 @@
+import sys
 from pdf_ingest import PdfIngest
 
-files = ["data/pdf/KEM_T2018_T528.pdf",
-         "data/pdf/KEM_T2030_T522.pdf",
-         "data/pdf/KEM_T2076_T52X-530.pdf",
-         "data/pdf/KEM_T2079_SSD.pdf",
-         "data/pdf/KEM_T2085_F-PS.pdf"]
+
+filename = sys.argv[1]
+with open(filename, "r") as f:
+    files = [line.strip('\n') for line in f]
 
 for file in files:
     pdf2db = PdfIngest(file)
     pdf2db.pdf2elements()
-    pdf2db.clean_elements()
+#    pdf2db.clean_elements()
     pdf2db.elements2chunks()
     pdf2db.chunks2docs()
     pdf2db.docs2db("./mydb")
